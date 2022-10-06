7cf415c1a67eda7794e968e6b3389175ced99740
janaSunrise <warriordefenderz@gmail.com>
2022-10-06 21:10:02 +0530

    Prepare for new version publish

--- packages/eviate-plugin/src/interfaces/plugin-events.ts
+++ packages/eviate-plugin/src/interfaces/plugin-events.ts
@@ -1,6 +1,4 @@
 export const PluginEvents = Object.freeze({
   // Event: when loaded in the app
-  Load: 'load',
-  // Event: when ready to run after app is ready
-  Ready: 'ready'
+  Load: 'load'
 });
